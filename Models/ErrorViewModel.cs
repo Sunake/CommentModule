fc5dfeb0adc0d1f9061b56c79b8f06a5b80e74f0
Роберт Авзанов <sunake4@gmail.com>
2020-10-30 14:18:54 +0500

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CommentModule.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
